274441d712974dd405b7772f7a4e4e67fb3b05ce
rich <richard.w.harris@mac.com>
2020-04-26 22:40:46 +0100

    playing around with Population.js

--- src/Population.js
+++ src/Population.js
@@ -1,14 +1,11 @@
 const State = Object.freeze({
-    'Susceptible': 0,
-    'Exposed': 1,
-    'Infected': 2,
-    'Symptomatic': 3,
-    'Removed': 4
+    Susceptible: 0,
+    Exposed: 1,
+    Infected: 2,
+    Symptomatic: 3,
+    Removed: 4
 })
 
-export let index_as_id = (individual, index) => {
-    return {id: index};
-}
 
 export let unit_square_distribution = () => {
     return {x: Math.random(), y: Math.random()};
@@ -16,20 +13,25 @@ export let unit_square_distribution = () => {
 
 export class Population {
 
-    constructor(population = []) {
-        this.population = Array.from(population)
+    constructor(individuals) {
+        this.individuals = Array.from(individuals)
+        this.individuals.forEach((individual) => {
+            if (individual.hasOwnProperty('id')) {
+                this[individual.id] = individual
+            }
+        })
     }
 
-    static of(count) {
-        return new Population(Array.from({length: count}, () => {
-                return {state: State.Susceptible};
-            }
-        ));
+    static of(count, idfn = (index) => index) {
+        return new Population(Array.from({length: count}, (individual, index) => {
+            let id = idfn(index)
+            return {state: State.Susceptible, id: id}
+        }));
     }
 
     with(fn) {
-        this.population.forEach((individual, index) => {
-            Object.assign(individual, fn(individual, index));
+        this.individuals.forEach((individual, id) => {
+            Object.assign(individual, fn(individual, id));
         })
         return this;
     }
