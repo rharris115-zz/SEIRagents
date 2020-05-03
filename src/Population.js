e568bc9b829f82cc06f5116d8d7bf2144d322197
rich <richard.w.harris@mac.com>
2020-05-03 15:51:43 +0100

    small things

--- src/Population.js
+++ src/Population.js
@@ -24,9 +24,9 @@ export class Population {
         Object.freeze(this) //Shallow freeze ... individuals are still mutable.
     }
 
-    static of(count, statefn = State.SUSCEPTIBLE.asSupplier(), idfn = index => index) {
+    static of(count, idfn = index => index) {
         return new Population(Array.from({length: count},
-            (agent, index) => ({state: statefn(), id: idfn(index)})
+            (agent, index) => ({state: State.SUSCEPTIBLE, id: idfn(index)})
         ))
     }
 
--- src/State.js
+++ src/State.js
@@ -7,10 +7,6 @@ class StateInstance {
         return (agent) => (agent.state === this)
     }
 
-    asSupplier() {
-        return () => this
-    }
-
     apply(agents) {
         for (let agent of agents) {
             agent.state = this
@@ -18,11 +14,9 @@ class StateInstance {
     }
 }
 
-export const State = {
+export const State = Object.freeze({
     SUSCEPTIBLE: Object.freeze(new StateInstance('s')),
     EXPOSED: Object.freeze(new StateInstance('e')),
     INFECTED: Object.freeze(new StateInstance('i')),
     REMOVED: Object.freeze(new StateInstance('r'))
-}
-
-Object.freeze(State)
+})
