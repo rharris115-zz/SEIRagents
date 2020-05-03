99d5065a7adebdb2216443fbed42c4cc2806e037
rich <richard.w.harris@mac.com>
2020-05-03 17:18:10 +0100

    improve html

--- src/State.js
+++ src/State.js
@@ -14,9 +14,24 @@ class StateInstance {
     }
 }
 
-export const State = Object.freeze({
+export const State = {
     SUSCEPTIBLE: Object.freeze(new StateInstance('s')),
     EXPOSED: Object.freeze(new StateInstance('e')),
     INFECTED: Object.freeze(new StateInstance('i')),
     REMOVED: Object.freeze(new StateInstance('r'))
-})
+}
+
+State.groupByCode = function (agents) {
+    let grouped = Object.values(State)
+        .reduce((a, b) => {
+            return {...a, [b.code]: []}
+        }, {})
+    for (let agent of agents) {
+        let code = agent.state.code
+        grouped[code].push(agent)
+    }
+    return Object.freeze(grouped)
+}
+
+
+Object.freeze(State)
